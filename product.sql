8158071e00a692e6662f4b4e6f956eeb97c97d04
Cindy Soares <cindy.as@gmail.com>
2022-09-26 08:38:56 -0300

    Update product.sql
    deterministic id

--- product.sql
+++ product.sql
@@ -1,8 +1,8 @@
-
 SELECT 
     stg.productname as mdmname,
     CONCAT("qawork-", stg.productname) as mdmdescription,
     stg.productprice as mdmunitvalue,
-    CONTAINS_SUBSTR(stg.productname, 'banan') AS mdmDeleted
-    --metadata--
+    CONTAINS_SUBSTR(stg.productname, 'flo') AS mdmDeleted,
+    to_hex(md5(lower(CONCAT(stg.mdmTenantId,'mdmproduct{"mdmname":"', stg.productname, '"}')))) __mdmId
+    --metadataNoId--
 from stg_nlp_product as stg
