a15789803905a92b85db0b28c59d5a9134c2d0dd
Cindy Soares <cindy.as@gmail.com>
2023-03-22 11:02:38 -0300

    Update item.sql

--- item.sql
+++ item.sql
@@ -1,5 +1,6 @@
 select
-  stg.id as mdmproducttype,
-  stg.name as mdmname
+  stg.cod_emitente as mdmproducttype,
+  --stg.id as mdmproducttype,
+  --stg.name as mdmname
   --metadata--
 from stg_datasul_item as stg
