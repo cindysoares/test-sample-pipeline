0f4e92c4481096302b746775f11ffeb87ac1d59d
Cindy Soares <cindy.as@gmail.com>
2024-03-15 16:53:28 -0300

    invalid type value

--- item.sql
+++ item.sql
@@ -1,5 +1,6 @@
 select
-  stg.cod_emitente as mdmproducttype
+  stg.cod_emitente as mdmproducttype,
+  stg.name as mdmtotalvalue
   --stg.id as mdmproducttype,
   --stg.name as mdmname
   --metadata--
