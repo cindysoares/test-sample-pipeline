27e138fc23c7b67194a510a9fccedaf671977f26
Cindy Soares <cindy.as@gmail.com>
2024-03-20 11:27:37 -0300

    Update item.sql

--- item.sql
+++ item.sql
@@ -1,9 +1,10 @@
 select
   stg.cod_emitente as mdmproducttype,
-  CASE
-   WHEN stg.cod_emitente = '987' THEN stg.cidade
-    ELSE CAST(10 as STRING)
-  END as mdmtotalvalue
+  stg.cidade as mdmtotalvalue
+  --CASE
+  -- WHEN stg.cod_emitente = '987' THEN stg.cidade
+  --  ELSE CAST(10 as STRING)
+  --END as mdmtotalvalue
   --stg.id as mdmproducttype,
   --stg.name as mdmname
   --metadata--
